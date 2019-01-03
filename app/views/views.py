d8fa681b01707a4f9a5916790dbdba8506e616e6
nanfuka <kalungi2k6@gmail.com>
2019-01-03 08:50:11 +0300

    Refactor code

--- app/views/views.py
+++ app/views/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, json
-from flask_swagger_ui import get_swaggerui_blueprint
+# from flasgger import Swagger, swag_from
 from app.controllers.incident_cont import Redflag
 from app.models.incident import Incident, incidents
 

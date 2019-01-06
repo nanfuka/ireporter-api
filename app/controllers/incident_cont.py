f11c31b1d91f92efb09c1fab6c9737235375b7cc
nanfuka <kalungi2k6@gmail.com>
2019-01-07 00:30:47 +0300

    Refactor code

--- app/controllers/incident_cont.py
+++ app/controllers/incident_cont.py
@@ -12,7 +12,6 @@ class Redflag():
         self.createdby = args[0]
         self.redflag = args[1]
         self.intervention = args[2]
-        # self.incident_type =[self.redflag, self.intervention]
         self.location = args[3]
         self.status = args[4]
         self.images = args[5]
@@ -26,12 +25,14 @@ class Redflag():
         return newinput
 
 
-    # def validate_input(self, createdby, location,redflag, intervention):
-    def validate_input(self, createdby, location, redflag, intervention,
-                       status):
+    def validate_input(self, *args):
+        createdby = args[0]
+        location = args[1]
 
-        # if createdby not in lst:
-        #     return "Createdby field must be present", 404
+        redflag = args[2]
+        intervention = args[3]
+        status = args[4]
+
 
         if not createdby or createdby.isspace():
             return 'please enter the id of the creator of this redflag'
@@ -44,11 +45,6 @@ class Redflag():
         elif status != "draft":
             return 'status should either be draft, underinvestigation, resolved or rejected'
 
-    # def validate_keys(self, createdby, location, lst):
-    #     if createdby not in lst:
-    #         return "All fields must be present", 404
-    #     elif not location or location.isspace():
-    #         return 'Email field can not be left empty.'
 
     def get_allredflags(self):
         if len(incidents) > 1:
@@ -74,56 +70,15 @@ class Redflag():
             if redflag['redflag_id'] == redflag_id:
                 redflag['location'] = data
                 return redflag
-            # return {"status": 404, "error":
-            #         "the record_id is not available"}
 
 
 
     def edits_record_location(self, redflag_id, item):
-        # data = request.get_json(['location'])
         record = [record for record in incidents if record['redflag_id'] == redflag_id]
         if record:
-            # return record
-#         return jsonify({"status": 404, "error": "no incident with such an id"}), 404
-# oneredflag = redflag.edit_redflag(redflag_id)
-#     if oneredflag:
             record[0][item] = request.json.get(item, record[0][item])
-    # if oneredflag[0]['location']:
             return jsonify({"status": 200, "data": [{"redflag_id": redflag_id, "message": "Updated redflag location"}]}), 200
-        # return jsonify({"status": 404, "error": "no incident with such an id"}), 404
-
-
-
-
-    # def edit_record_comments(self, redflag_id, newcomment):
-
-    #     record = [
-    #         record for record in incidents if record[
-    #             'redflag_id'] == redflag_id]
-
-    #     if record:
-    #         record['comment'] = newcomment
-
-    #         return jsonify({"status": 200, "data": record})
-
-        # return jsonify({"status": 404, "error":
-        #                 "the record_id is not available"})
-
-        # for redflag in incidents:
-        #     if redflag['redflag_id'] == redflag_id:
-        #         # redflag['comment'] = data
-        #         return redflag
-        # data = request.get_json(['comment'])
-        # for redflag in incidents:
-        #     if redflag['redflag_id'] == redflag_id:
-        #         redflag['comment'] = data
-        #     return redflag
-            #     if redflag:
-            #         return jsonify({"status": 201, "data": redflag})
-            #     return jsonify({"status": 404, "error":
-            #                    "the record_id is not available"})
-            # return {"status": 404, "error":
-            #                 "the record_id is not available"}                               
+                               
 
     def delete_record(self, redflag_id):
         record = [
@@ -137,31 +92,6 @@ class Redflag():
         return {"status": 404, "error":
                 "the record_id is not available"}
 
-
-
-    # def update_incident(self, incident_id, request_data, keyword, username):
-
-    #     location = request_data.get("location")
-
-    #     if self.validator.check_empty_string(location):
-    #         return self.response_unaccepted("empty")
-
-    #     if self.validator.check_str_datatype(location):
-    #         return self.response_unaccepted("datatype")
-
-    #     update_incident_instance = incident_data.update_incident(
-    #         incident_id, request_data, keyword, username)
-    #     if update_incident_instance is None:
-    #         return self.response_unaccepted("none")
-    #     elif update_incident_instance == "non_author":
-    #         return Response(json.dumps({
-    #             "status":401,
-    #             "message": "You are not authorised to edit this incident."
-    #         }), content_type="application/json", status=401)
-    #     else:
-    #         return self.response_sumission_success(update_incident_instance,
-    #                                                "update")
-
     def edit_redflag(self, redflag_id):
         """ 
         edit a particular redflags
@@ -171,10 +101,3 @@ class Redflag():
             return record
         return jsonify({"status": 404, "error": "no incident with such an id"}), 404
 
-    # def update_location(self, incident_id):
-    #     """ 
-    #     updates single  redflags
-    #     """
-    #     red_flag = [
-    #         incident for incident in incidents if incident['incident_id'] == incident_id]
-    #     return red_flag
